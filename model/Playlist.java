94efcbff7f9fd9e192ec9be674da733b22c78c56
SitiNursinta05 <shintaaini933@gmail.com>
2021-03-25 14:03:43 +0000

    Initial commit

---
+++ model/Playlist.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Playlist {
+  public String title;
+  public String owner;
+  public Playlist(String title, String owner) {
+    this.title = title;
+    this.owner = owner;
+  }
+}
